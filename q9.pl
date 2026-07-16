55dbaaac32f06c7dc7d50615acffe89121f1ee42
Debora <debora.a.caetano@edu.ufes.br>
2026-07-16 16:24:10 -0300

    q9

--- q9.pl
+++ q9.pl
@@ -14,4 +14,24 @@ estrada(d, f, 30).
 estrada(e, f, 26).
 
 % b
+%vamos construir uma logica recursiva,
+%se os caminhos n estao conectados, ele ve um intermediario e vai somando
+
+%caso base
+dist(A, B, D) :- estrada(A, B, D).
+dist(A, B, D) :- 
+    estrada(A, X, DNova),
+    dist(X, B, DResto),
+    D is DNova + DResto.
+
+rota(A, B, [A, B]) :- estrada(A, B, _).
+rota(A, B, [A|R]) :- 
+    estrada(A, X, _),
+    rota(X, B, R).
+
+%consultas
+% dist(a, b, D).
+% dist(a, c, D).
+% rota(a, b, R).
+% rota(a, f, R).
 
